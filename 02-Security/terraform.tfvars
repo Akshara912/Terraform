1685e879a3f238afcffbc0fa484e03047826867c
Developer1 <developer1@gmail.com>
2026-08-19 02:49:30 +0530

    adding my 2nd project

---
+++ 02-Security/terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_cidr = "10.0.0.0/16"
+environment = "production"
+subnet_cidr = "10.0.1.0/24"
+rule_port = ["22", "80"]
---
+++ 02-Security/varibale.tf
@@ -0,0 +1,6 @@
+variable "vpc_cidr" {}
+variable "environment" {}
+variable "subnet_cidr" {}
+variable "rule_port" {
+    type = list
+}
